1cdae590dc06ed5628bf53cc056aec0a988990e2
Bernardo Pires Mesko <bernardo.mesko@ip5tecnologia.com.br>
2024-03-04 21:25:51 -0300

    MSAL Labs

---
+++ labs/az204-lab03/Program.cs
@@ -0,0 +1,21 @@
+﻿using Azure.Storage;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+const string blobServiceEndpoint = "https://mediastgberb.blob.core.windows.net/";
+const string storageAccountName = "mediastgberb";
+const string storageAccountKey;
+
+StorageSharedKeyCredential accountCredentials = new StorageSharedKeyCredential(storageAccountName, storageAccountKey);
+
+BlobServiceClient serviceClient = new BlobServiceClient(new Uri(blobServiceEndpoint), accountCredentials);
+
+AccountInfo accountInfo = await serviceClient.GetAccountInfoAsync();
+
+await Console.Out.WriteLineAsync($"Connected to Azure Storage Account");
+
+await Console.Out.WriteLineAsync($"Account name:\t{storageAccountName}");
+
+await Console.Out.WriteLineAsync($"Account kind:\t{accountInfo?.AccountKind}");
+
+await Console.Out.WriteLineAsync($"Account sku:\t{accountInfo?.SkuName}");
---
+++ labs/learningpath-msal/az204-auth/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+const string _clientId = "25e0e726-abc8-41a6-9dca-3fb4ddf39071";
+const string _tenantId = "495a6f2f-e021-4750-b745-9b4282842206";
+
+var app = PublicClientApplicationBuilder
+    .Create(_clientId)
+    .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+    .WithRedirectUri("http://localhost")
+    .Build();
+
+string[] scopes = { "user.read" };
+
+AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+Console.WriteLine($"Token:\t{result.AccessToken}");
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
